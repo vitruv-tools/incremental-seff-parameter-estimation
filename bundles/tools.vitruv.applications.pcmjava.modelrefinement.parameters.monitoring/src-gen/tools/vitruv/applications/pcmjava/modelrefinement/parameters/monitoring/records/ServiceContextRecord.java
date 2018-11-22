a4fcf14eb7b5d7500a2bc393d1268db231561277
David Monschein <ugdzy@student.kit.edu>
2018-11-22 14:57:00 +0100

    Added records to project.

---
+++ bundles/tools.vitruv.applications.pcmjava.modelrefinement.parameters.monitoring/src-gen/tools/vitruv/applications/pcmjava/modelrefinement/parameters/monitoring/records/ServiceContextRecord.java
@@ -0,0 +1,12 @@
+package tools.vitruv.applications.pcmjava.modelrefinement.parameters.monitoring.records;
+
+
+/**
+ * @author Generic Kieker
+ * 
+ * @since 1.13
+ */
+public interface ServiceContextRecord extends RecordWithSession {
+	public String getServiceExecutionId();
+	
+}
