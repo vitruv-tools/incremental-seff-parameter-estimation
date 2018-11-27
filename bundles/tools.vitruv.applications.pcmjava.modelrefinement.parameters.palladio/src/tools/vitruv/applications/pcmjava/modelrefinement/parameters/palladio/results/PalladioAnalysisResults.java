d0274a078e74f9aa1cdf8b6b0b333905ab6c3d04
David Monschein <ugdzy@student.kit.edu>
2018-11-27 14:01:58 +0100

    Implemented experiment result deserialization.

---
+++ bundles/tools.vitruv.applications.pcmjava.modelrefinement.parameters.palladio/src/tools/vitruv/applications/pcmjava/modelrefinement/parameters/palladio/results/PalladioAnalysisResults.java
@@ -0,0 +1,45 @@
+package tools.vitruv.applications.pcmjava.modelrefinement.parameters.palladio.results;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import javax.measure.Measure;
+import javax.measure.quantity.Duration;
+
+import org.palladiosimulator.edp2.models.measuringpoint.MeasuringPoint;
+
+public class PalladioAnalysisResults {
+	private Map<MeasuringPoint, MeasuringPointResults> results;
+
+	public PalladioAnalysisResults() {
+		this.results = new HashMap<>();
+	}
+
+	public Set<Entry<MeasuringPoint, MeasuringPointResults>> entries() {
+		return results.entrySet();
+	}
+
+	public void addLongs(MeasuringPoint p, List<Measure<Long, Duration>> longs) {
+		init(p);
+		this.results.get(p).applyLongValues(longs);
+	}
+
+	public void addDoubles(MeasuringPoint p, List<Measure<Double, Duration>> doubles) {
+		init(p);
+		this.results.get(p).applyDoubleValues(doubles);
+	}
+
+	public Map<MeasuringPoint, MeasuringPointResults> getResults() {
+		return results;
+	}
+
+	private void init(MeasuringPoint p) {
+		if (!this.results.containsKey(p)) {
+			this.results.put(p, new MeasuringPointResults());
+		}
+	}
+
+}
