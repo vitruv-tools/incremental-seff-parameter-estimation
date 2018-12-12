7a35e71f8fadfbe7351954e8b91d1691e9c9b48e
David Monschein <david.monschein@novatec-gmbh.de>
2018-12-12 13:43:23 +0100

    Updated CoCoME PCM and uploaded missing files.

---
+++ bundles/tools.vitruv.applications.pcmjava.modelrefinement.parameters.load/src/tools/vitruv/applications/pcmjava/modelrefinement/parameters/load/test/ExecuteLoadTest.java
@@ -0,0 +1,17 @@
+package tools.vitruv.applications.pcmjava.modelrefinement.parameters.load.test;
+
+import java.io.IOException;
+
+import tools.vitruv.applications.pcmjava.modelrefinement.parameters.load.JMeterExecutor;
+
+public class ExecuteLoadTest {
+
+	@org.junit.Test
+	public void test() throws IOException {
+		JMeterExecutor exec = new JMeterExecutor(
+				"/Users/david/Desktop/Studium/WS1819/Praktikum IngSoftware/Apache JMeter/apache-jmeter-5.0");
+
+		exec.execute();
+	}
+
+}
